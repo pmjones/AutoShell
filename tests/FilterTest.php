2bc97f2ea33578af4de3b46229d726c27bf04225
Jonathon Hill <jonathon@compwright.com>
2022-11-10 11:34:44 -0500

    fix: Add missing strict_types

--- tests/FilterTest.php
+++ tests/FilterTest.php
@@ -1,4 +1,6 @@
 <?php
+declare(strict_types=1);
+
 namespace AutoShell;
 
 class FilterTest extends \PHPUnit\Framework\TestCase
--- tests/ManualTest.php
+++ tests/ManualTest.php
@@ -1,4 +1,6 @@
 <?php
+declare(strict_types=1);
+
 namespace AutoShell;
 
 class ManualTest extends \PHPUnit\Framework\TestCase
