3240283ca34aa999a5d9ee2a266282a45f75e6cd
Paul M. Jones <pmjones88@gmail.com>
2022-12-14 20:23:15 -0600

    rename fake console script

--- tests/Fake/console.php
+++ tests/Fake/console.php
@@ -4,7 +4,7 @@ use AutoShell\Console;
 require dirname(__DIR__, 2) . '/vendor/autoload.php';
 
 $console = Console::new(
-    namespace: 'AutoShell\\Fake\\Command',
+    namespace: 'AutoShell\Fake\Command',
     directory: __DIR__ . '/Command'
 );
 
